6791336ed8312093f0d5f00a027dd5f230a02337
Elias Silva <eliasmanuelsilva@hotmail.com>
2016-09-27 17:25:24 +0100

    First message display

--- app/src/main/java/com/example/android/insult/MainActivity.java
+++ app/src/main/java/com/example/android/insult/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.insult;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void randomInsult(View view) {
+        display("Vai passear");
+    }
+
+    public void randomPraise(View view) {
+        display("Que bonito!");
+    }
+
+    private void display(String message) {
+        TextView quantityTextView = (TextView) findViewById(R.id.message);
+        quantityTextView.setText(message);
+    }
+
 }
