4d6a858e7e2fb10cd48e9313a976f825ee5ad625
Joana Contente <joanacontente@gmail.com>
2016-09-29 10:39:37 +0100

    added audioButton

--- app/src/main/java/com/example/android/insult/MainActivity.java
+++ app/src/main/java/com/example/android/insult/MainActivity.java
@@ -5,8 +5,6 @@ import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,13 +24,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=(ImageButton) findViewById(R.id.insulta);
+        b1=(ImageButton) findViewById(R.id.audioButton);
 
         t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status != TextToSpeech.ERROR) {
-                    t1.setLanguage(Locale.UK);
+                    t1.setLanguage(new Locale("pt"));
                 }
             }
         });
@@ -64,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void randomCompliment(View view) {
         Resources res = getResources();
-        myString = res.getStringArray(R.array.praises);
+        myString = res.getStringArray(R.array.compliments);
         stringToSpeech = myString[rgenerator.nextInt(myString.length)];
         display(stringToSpeech);
     }
